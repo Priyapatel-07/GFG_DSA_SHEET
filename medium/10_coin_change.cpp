52379b8f5b7bcdb2c3581131241bf5fec9f7fa44
Priya Patel <69634511+Priyapatel-07@users.noreply.github.com>
2022-08-24 23:44:57 +0530

    Dsasheet (#6)
    * nbcnb
    
    * add file
    
    * alien dictonary
    
    * update README.md
    
    * corrections
    
    * more files
    
    * add file
    
    * new files
    
    * changes

---
+++ medium/10_coin_change.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    long long int count(int S[], int m, int n) {
+
+        // code here.
+      vector<vector<long long int>> dp(m+1, vector<long long int> (n+1));
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0) dp[i][j]=0;
+                if(j==0) dp[i][j]=1;
+            }
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(S[i-1]<=j){
+                    dp[i][j]=dp[i][j-S[i-1]]+dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int arr[m];
+        for (int i = 0; i < m; i++) cin >> arr[i];
+        Solution ob;
+        cout << ob.count(arr, m, n) << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
---
+++ medium/8_spirally_traversing_a_matrix.cpp
@@ -0,0 +1,17 @@
+for(int i=0; i<r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
